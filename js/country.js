ddea4c566ef9f4511be011cb44a9cbc819461b8f
Shay <shaybush93@gmail.com>
2022-08-07 13:09:56 +0300

    try

--- js/country.js
+++ js/country.js
@@ -8,7 +8,7 @@ class Country{
         this.region = _item.region;
         this.capital = _item.capital;
         this.index = _index;
-        console.log(this.index)
+        console.log(this.capital);
         this.render();
     }
     render(){
