94ec8ec0c1033e4fb1898c553c3189c502305cd9
shimislm <shimislm5@gmail.com>
2022-08-07 21:50:56 +0300

    score table update

--- js/highScores.js
+++ js/highScores.js
@@ -4,13 +4,15 @@ const init = () => {
 }
 const updateUi = () => {
     obj_scores = JSON.parse(localStorage["scoreListCapital"])
+    // console.log(obj_scores)
+    obj_scores = _.reverse(_.sortBy(obj_scores , "points"));
     document.querySelector("#id_tbody").innerHTML = "";
-    obj_scores.forEach(element => {
+    obj_scores.forEach((element, index) => {
       let tr = document.createElement("tr");
       tr.innerHTML = `
         <td>${element.fullName}</td>
         <td>${element.points}</td>
-        <td>Top 1</td>
+        <td>Top ${index+1}</td>
       `
       document.querySelector("#id_tbody").append(tr);
     });
